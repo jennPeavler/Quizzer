2a9fdd5eaa639952ce1d328c7791d1f5bf761feb
Jennifer Peavler <jenn.peavler@gmail.com>
2017-06-01 09:53:47 -0600

    add switch user to stateful

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,29 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export const Quiz = (props) => {
-  console.log(props)
-  return (
-    <form>
-      <h1>{props.quiz.title}</h1>
-      <section className='questions'>
-        {props.quiz.questions.map((question, i) => {
-          return (
-            <section key={i}>
-              <h4 key={question.id}>{question.title}</h4>
-              <div>
-                {question.answers.map((answer, j) => {
-                  return (
-                    <div className='answers' key={j}>
-                      <input type='radio' name={question.id} />  {answer.title}<br />
-                    </div>
-                  )
-                })}
-              </div>
-            </section>
-          )
-        })}
-      </section>
-      <button id='submit-btn'>Submit</button>
-    </form>
-  )
+export class Quiz extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <h1>{this.props.quiz.title}</h1>
+        <section className='questions'>
+          {this.props.quiz.questions.map((question, i) => {
+            return (
+              <section key={i}>
+                <h4 key={question.id}>{question.title}</h4>
+                <div>
+                  {question.answers.map((answer, j) => {
+                    return (
+                      <div className='answers' key={j}>
+                        <input type='radio'
+                               name={question.id}
+                               onClick={event => {
+                                 this.submitAnswer(answer.score, question.id)
+                               }}
+                        />
+                          {answer.title}<br />
+                      </div>
+                    )
+                  })}
+                </div>
+              </section>
+            )
+          })}
+        </section>
+        <button id='submit-btn'>
+          Submit
+        </button>
+      </form>
+    )
+  }
 }
